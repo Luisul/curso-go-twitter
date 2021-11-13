897681631f1421094487585b4951393ac4b2d280
Luis uribe lozano <luisuribelozano@gmail.com>
2021-11-12 22:52:59 -0500

    Ajuste middle

--- middlew/validoJWT.go
+++ middlew/validoJWT.go
@@ -8,11 +8,17 @@ import (
 
 /*ValidoJWT permiye validar el JWT en las peticiones*/
 func ValidoJWT(next http.HandlerFunc) http.HandlerFunc {
+
 	return func(rw http.ResponseWriter, r *http.Request) {
+
 		_, _, _, err := routers.ProcesoToken(r.Header.Get("Authorization"))
+
 		if err != nil {
+
 			http.Error(rw, "Error en el Token !!"+err.Error(), http.StatusBadRequest)
 			return
 		}
+
+		next.ServeHTTP(rw, r)
 	}
 }
